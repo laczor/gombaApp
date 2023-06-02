288e1e4176239ff320e04542118717a77510a8a3
Istvan Laczovics <istvanl@topdesk.com>
2023-06-02 16:35:41 +0200

    Delete a marker

--- markers.js
+++ markers.js
@@ -3,12 +3,34 @@ export function Markers({ api, map }) {
 
     function addMarker({lat,lng, name, id}) {
         var marker = L.marker([lat, lng]).addTo(map);
+        console.log(marker)
         markers.push(marker);
-        marker.bindPopup(`<div>${name}</div><br><button onClick="deleteMarker(${id})">Delete</button>`);
+        marker.location_id = id;
+        marker.bindPopup(`<div>${name}</div><br><button onClick="deleteMarker({id: ${id}, 'name': '${name}'})">Delete</button>`);
+    }
+
+    async function deleteMarker({name, id}) {
+
+        let confirmAction = confirm(`Biztos törlöd a következőt? \n ${name}`);
+        if (confirmAction) {
+            try {
+                await api.deleteLocation(id);
+                const markerToBeDeleted = markers.find((marker) => {
+                    return marker.location_id === id;
+                });
+                if(markerToBeDeleted){
+                    map.removeLayer(markerToBeDeleted);
+                }
+            } catch (error) {
+                alert("Nem tudtam törölni");
+
+            }
+        }
     }
 
     return {
         addMarker,
+        deleteMarker,
     }
 
 }
--- todo.md
+++ todo.md
@@ -2,12 +2,11 @@
 0.1 Add current location to the map with name. [.]
 0.2 Add location from map with name [.]
 1. Draw all of the markers from the db, show their name.[.]
-2. Delete marker, from db and from map.
+2. Delete marker, from db and from map. [.]
     - When we would like to delete, we open the options.
     - It will add an extra deletion button to the markers.
     - Will open a popup for deletion.
     - if yes, will remove the marker and the database entry.
-    - We can delete multiple markers, when we fiinished we have to go back to the options to finish deleting.
 3. Modify an existing entry.
 4. Export all of the data in a txt file.
 5. Publish it to an online space.
