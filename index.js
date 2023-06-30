1da98aeecccb4ccd9b5b6750f13fc6ff894824ce
Istvan Laczovics <istvanl@topdesk.com>
2023-06-30 11:05:29 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+import { dbPromise } from 'db'
+import { Api } from 'api';
+import { Map } from 'map';
+import { isLocation } from 'domain';
+import { Markers } from 'markers';
+
+import { SaveLocationModal } from 'save-location-form'
+import { AddLocationsMenu } from 'add-locations-menu'
+import { exportData } from 'export'
+import { importData } from 'import'
+
+const api = Api(dbPromise);
+const { map } = Map(L);
+map.markInitialLocation();
+const markers = Markers({ api, map })
+
+
+const saveLocationModal = SaveLocationModal({ api, modalElement: ADD_CURRENT_LOCATION_MODAL, addMarker: markers.addMarker, closeMarker: markers.closeMarker });
+saveLocationModal.render();
+
+AddLocationsMenu(
+    {
+        menuElement: ADD_LOCATIONS_MENU,
+        openModal: saveLocationModal.openModal,
+        map,
+        exportData: exportData({ getData: api.getAllLocations}),
+        importData: importData({saveData: api.saveLocation, validator: isLocation}),
+}).render();
+
+window.deleteMarker = markers.deleteMarker;
+window.opeSaveLocationModal = saveLocationModal.openModal;
+
+api.getAllLocations().then((locations) => {
+        locations.map(markers.addMarker);
+});
